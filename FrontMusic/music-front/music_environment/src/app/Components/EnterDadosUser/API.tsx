a789a0a7d8dd67fbfa460b4588f72482a0acb5ea
jhonnathan wesley lopes <90536096+Jhowzao24@users.noreply.github.com>
2026-08-10 20:06:49 -0300

    Update API base URL to Render deployment

--- FrontMusic/music-front/music_environment/src/app/Components/EnterDadosUser/API.tsx
+++ FrontMusic/music-front/music_environment/src/app/Components/EnterDadosUser/API.tsx
@@ -2,20 +2,20 @@ import axios from 'axios';
 import { User } from '../CRUDEnterDatas';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/Urls/ViewsStudy/',
-  });
-  
-  export const fetchUsers = async (): Promise<User[]> => {
-    const response = await api.get('http://127.0.0.1:8000/Urls/ViewsStudy/');
-    if (Array.isArray(response.data)) {
-        return response.data;
-      } else {
-        console.error('API response is not an array:', response.data); // Adicione esta linha para depuração
-        return [];
-      }
-  };
-  
-  export const updateUser = async (user: User): Promise<User> => {
-    const response = await api.put(`http://127.0.0.1:8000/Urls/ViewsStudy/${user.id}`, user);
+  baseURL: 'https://app-django-musica.onrender.com',
+});
+
+export const fetchUsers = async (): Promise<User[]> => {
+  const response = await api.get('https://app-django-musica.onrender.com/Urls/ViewsStudy/');
+  if (Array.isArray(response.data)) {
     return response.data;
-  };
+  } else {
+    console.error('API response is not an array:', response.data);
+    return [];
+  }
+};
+
+export const updateUser = async (user: User): Promise<User> => {
+  const response = await api.put(`https://app-django-musica.onrender.com/Urls/ViewsStudy/${user.id}`, user);
+  return response.data;
+};
